71da790e9c51ea43094a9898287df01b52307375
Sam O'Cain <samocain93@gmail.com>
2023-06-24 21:51:56 -0500

    setting up react router routes for home and login pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,50 +1,21 @@
 import './App.css';
+import Post from './components/Post.js';
+import Header from './components/Header';
+import Layout from './Layout';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
-
-  const today = new Date()
-
   return (
     <>
-      <main>
-        <header>
-          <a href='' className='logo'>
-            My Blog
-          </a>
-          <nav>
-            <a href=''>Login</a>
-            <a href=''>Register</a>
-          </nav>
-        </header>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<Post />} />
 
-        <div className='post'>
-          <div className='image'>
-            <img
-              src='https://techcrunch.com/wp-content/uploads/2017/03/cyclops2-2017-0106-underwater.jpg?w=990&crop=1'
-              alt='oceangate sub'
-            />
-          </div>
+          <Route path={'/login'} element={<div>Login Page</div>}></Route>
+        </Route>
 
-          <div className='content'>
-            <h2>
-              OceanGate fires a whistleblower, hackers threaten to leak Reddit
-              data, and Marvel embraces AI art
-            </h2>
-            <div className="info">
-              <a className='author'>Sam O'Cain</a>
-              <time>2023-06-23 9:47am</time>
-            </div>
-            <p className='summary'>
-              OceanGate fired a whistleblower: The director of marine operations
-              at OceanGate, the company whose submersible went missing Sunday on
-              an expedition to the Titanic in the North Atlantic, was fired
-              after raising concerns about its first-of-a-kind carbon fiber hull
-              and other systems before its maiden voyage, according to a court
-              filing in a 2018 lawsuit.
-            </p>
-          </div>
-        </div>
-      </main>
+        <Route path={'/register'} element></Route>
+      </Routes>
     </>
   );
 }
---
+++ client/src/components/Post.js
@@ -0,0 +1,31 @@
+export default function Post() {
+  return (
+    <div className='post'>
+      <div className='image'>
+        <img
+          src='https://techcrunch.com/wp-content/uploads/2017/03/cyclops2-2017-0106-underwater.jpg?w=990&crop=1'
+          alt='oceangate sub'
+        />
+      </div>
+
+      <div className='content'>
+        <h2>
+          OceanGate fires a whistleblower, hackers threaten to leak Reddit data,
+          and Marvel embraces AI art
+        </h2>
+        <div className='info'>
+          <a className='author'>Sam O'Cain</a>
+          <time>2023-06-23 9:47am</time>
+        </div>
+        <p className='summary'>
+          OceanGate fired a whistleblower: The director of marine operations at
+          OceanGate, the company whose submersible went missing Sunday on an
+          expedition to the Titanic in the North Atlantic, was fired after
+          raising concerns about its first-of-a-kind carbon fiber hull and other
+          systems before its maiden voyage, according to a court filing in a
+          2018 lawsuit.
+        </p>
+      </div>
+    </div>
+  );
+}
