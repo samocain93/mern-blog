6b3bafdfedb826ae28ad9bac20e1cf2c3f0243ac
Sam O'Cain <samocain93@gmail.com>
2023-06-26 12:22:23 -0500

    added login and register styling

--- client/src/App.js
+++ client/src/App.js
@@ -3,18 +3,19 @@ import Post from './components/Post.js';
 import Header from './components/Header';
 import Layout from './Layout';
 import { Route, Routes } from 'react-router-dom';
+import IndexPage from './pages/IndexPage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
 
 function App() {
   return (
     <>
       <Routes>
         <Route path='/' element={<Layout />}>
-          <Route index element={<Post />} />
-
-          <Route path={'/login'} element={<div>Login Page</div>}></Route>
+          <Route index element={<IndexPage />} />
+          <Route path='/login' element={<LoginPage />}></Route>
+          <Route path='/register' element={<RegisterPage />}></Route>
         </Route>
-
-        <Route path={'/register'} element></Route>
       </Routes>
     </>
   );
